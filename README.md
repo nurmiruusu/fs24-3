dea95688fbcd5fe72d163a52f02567c37fab97c8
nurmiruusu <nurmiruusu@gmail.com>
2024-02-13 18:05:50 +0200

    frontend

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # fs24-3
+
+## Online backend
+### https://fs-phonebook-ml4l.onrender.com/
--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
-app.use(morgan('tiny'));
+app.use(morgan('tiny'))
+app.use(express.static('dist'))
 
 morgan.token('type', function (req, res) { return req.headers['content-type'] })
 
